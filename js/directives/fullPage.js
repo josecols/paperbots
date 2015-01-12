de8fb16f5417639565e13a20ebce86ba9648c0da
José Cols <josecolsg@gmail.com>
2015-01-12 18:31:44 -0430

    Diseño responsive

--- js/directives/fullPage.js
+++ js/directives/fullPage.js
@@ -1,12 +1,15 @@
 (function () {
 
   var app = angular.module('paperbots');
-  
+
   app.directive("fullPage", function ($window) {
     return {
       restrict: "A",
       link: function (scope, element, attrs) {
         angular.element(element).css('height', $window.innerHeight);
+        angular.element($window).on('resize', function () {
+          angular.element(element[0].getElementsByClassName('section')).css('height', $window.innerHeight);
+        });
       }
     };
   });
